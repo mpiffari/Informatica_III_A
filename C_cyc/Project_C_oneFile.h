b54c8c85870ed554d2624f3795ed9c1005181c42
Piffo <michele.piffari@gmail.com>
2020-02-24 12:25:31 +0100

    Simplified cyclone code deleting opaque types

--- C_cyc/Project_C_oneFile.h
+++ C_cyc/Project_C_oneFile.h
@@ -2,9 +2,9 @@
 #define PROJECT_C_ONEFILE_H_
 
 //typedef struct date@ date_ref;
-typedef struct postcard@? postcard_ref;
+//typedef struct postcard@? postcard_ref;
 
-postcard_ref make_postcard(char@); // Make postcard from file line reading
-void printPostcard(postcard_ref);
+//postcard_ref make_postcard(char@); // Make postcard from file line reading
+//void printPostcard(postcard_ref);
 
 #endif /* PROJECT_C_ONEFILE_H_ */
