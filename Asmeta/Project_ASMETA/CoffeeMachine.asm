34ed27dbe09a4b16db3f770c39c10c075fb1ffe4
Piffo <michele.piffari@gmail.com>
2020-03-01 16:45:57 +0100

    ASM completed

--- Asmeta/Project_ASMETA/CoffeeMachine.asm
+++ Asmeta/Project_ASMETA/CoffeeMachine.asm
@@ -8,29 +8,22 @@ signature:
 
 	domain CoinValue subsetof Integer						// Coins accepted by the machine (value in cents)
 
-	enum domain State = {WAITING |
-						COINS_INSERTED | GIVING_CHANGE |
-						PREPARED | MAINTAINING |
-						OUT_OF_SERVICE}						// Possible states for the machine
-	enum domain WaitingAction = {INSERT_COINS |
-								PRODUCT_INFO | MAINTAIN |
-								EXIT}						// Possible user actions when the machine is waiting
-	enum domain CoinsInsAction = {MORE_COINS |
-								CHOOSE_PRODUCT}				// Possible user actions when some coins have been inserted
-	enum domain PreparedAction = {PICK}						// Possible user actions when the chosen  has been prepared
-	enum domain OutOfServiceAction = {REPAIR}				// Possible user actions when the machine is out of service
-	enum domain MaintainAction = {FILL | SET_COINS |
-									FINISH}					// Possible user actions when the machine has been opened for maintenance
-	enum domain Ingredient = {COFFEE | MILK | WATER |
-							TEA | CHOCOLATE |
-							PLASTIC_GLASS}					// The "ingredients" needed to prepare a product
+	enum domain State = {WAITING | COINS_INSERTED | GIVING_CHANGE | PREPARED | MAINTAINING | OUT_OF_SERVICE}						// Possible states for the machine
+
+	enum domain WaitingAction = {INSERT_COINS | PRODUCT_INFO | MAINTAIN | EXIT}	// Possible user actions when the machine is waiting
+	enum domain CoinsInsAction = {MORE_COINS | CHOOSE_PRODUCT}				    // Possible user actions when some coins have been inserted
+	enum domain PreparedAction = {PICK}						                    // Possible user actions when the chosen  has been prepared
+	enum domain OutOfServiceAction = {REPAIR}				                    // Possible user actions when the machine is out of service
+	enum domain MaintainAction = {FILL | FILL_COINS | FINISH}					// Possible user actions when the machine has been opened for maintenance
+
+	enum domain Ingredient = {COFFEE | MILK | WATER | TEA | CHOCOLATE | PLASTIC_GLASS}					// The "ingredients" needed to prepare a product
 
 
 	/****** MONITORED FUNCTIONS (system readble) ******/
 
 	dynamic monitored selectedProduct	 : Product				// The product that has just been chosen
 	dynamic monitored insertedCoin		 : CoinValue			// The coin that has just been inserted
-	dynamic monitored numberOfCoins		 : Natural				// The number of coins left in the machine by the maintenance man
+	dynamic monitored numberOfCoins		 : Integer				// The number of coins left in the machine by the maintenance man
 	dynamic monitored filledIngredient	 : Ingredient			// The ingredient whose container has to be filled by the maintenance man
 	dynamic monitored waitingAction		 : WaitingAction		// The selected action in the WAITING state
 	dynamic monitored coinsInsAction	 : CoinsInsAction		// The selected action in the COINS_INSERTED state
@@ -44,8 +37,8 @@ signature:
 	dynamic controlled state		    			: State					// The machine state
 	dynamic controlled display		    			: String				// The message displayed to the user
 	dynamic controlled credit		    			: Integer				// The current credit of the user (coins only)
-	dynamic controlled coinsLeft	    			: CoinValue -> Natural	// For each coin value, the number of coins held by the machine
-	dynamic controlled maxInseartableCoinsQuantity	: CoinValue -> Natural	// For each coin value, the max quantity of coins that the machine can store
+	dynamic controlled coinsLeft	    			: CoinValue -> Integer	// For each coin value, the number of coins held by the machine
+	dynamic controlled maxSpaceAvailable			: CoinValue -> Integer	// For each coin value, the max quantity of coins that the machine can store
 	dynamic controlled quantityLeft     			: Ingredient -> Integer	// The amount of ingredient left in the machine
 
 
@@ -54,8 +47,6 @@ signature:
 	static capacity			: Ingredient -> Integer					// The maximum amount of an ingredient that the machine can hold
 	static quantityNeeded	: Prod(Product, Ingredient) -> Integer	// The amount of ingredient needed to prepare a product
 	static price			: Product -> Integer					// The price of a product
-	static currencyToString : Integer -> String						// Formats an amount of cents in a human readable string
-
 
 	/****** DERIVED FUNCTIONS ******/
 
@@ -122,9 +113,6 @@ definitions:
 			case glassOnly	: 10
 		endswitch
 
-	function currencyToString($n in Integer) =
-		concat(toString($n/100), " €")
-
 	/****** DERIVED FUNCTIONS ******/
 
 	function canPrepare($p in Product) =
@@ -153,7 +141,7 @@ definitions:
 				case WAITING:
 					display := "Waiting..."
 				case COINS_INSERTED:
-					display := concat("Credit: ", currencyToString(credit) )
+					display := concat("Credit: ", concat(toString(credit/100), " €"))
 				case PREPARED:
 					display := "Prepared."
 				case MAINTAINING:
@@ -170,7 +158,7 @@ definitions:
 	rule r_productInfo =
 		let($p1 = selectedProduct) in
 			if(canPrepare($p1)) then
-				display := currencyToString(price($p1))
+				display := concat(toString((price($p1))/100), " €")
 			else
 				display := "not available"
 			endif
@@ -191,7 +179,7 @@ definitions:
 		let($c = insertedCoin) in
 			seq
 				credit := credit + $c
-				coinsLeft($c) := coinsLeft($c) + 1n
+				coinsLeft($c) := coinsLeft($c) + 1
 				r_changeState[COINS_INSERTED]
 			endseq
 		endlet
@@ -214,7 +202,7 @@ definitions:
 						r_consumeIngredients[$p2]
 					endpar
 				else
-					display := concat("price is ", currencyToString( price($p2) ) )
+					display := concat("price is ", concat(toString((price($p2))/100), " €"))
 
 				endif
 			endif
@@ -224,17 +212,17 @@ definitions:
 	rule r_giveChange =
 		if(state = GIVING_CHANGE) then
 			// Find the highest available coin value, not greater than the residual credit
-			choose $cv in CoinValue with coinsLeft($cv) > 0n // Available one
+			choose $cv in CoinValue with coinsLeft($cv) > 0 // Available one
 			and $cv <= credit  // Lower then credit
-			and	not(exist $cv2 in CoinValue with coinsLeft($cv2) > 0n and $cv2 <= credit and $cv2 > $cv) do // The highest coin possible
+			and	not(exist $cv2 in CoinValue with coinsLeft($cv2) > 0 and $cv2 <= credit and $cv2 > $cv) do // The highest coin possible
 				let ($cl = coinsLeft($cv)) in
-					if($cl <= 0n) then
+					if($cl <= 0) then
 						display := "ERROR"
 					else
 						seq
 							credit := credit - $cv
-							//coinsLeft($cv) := $cl - 1
-							display := concat("Pick up your change! Credit on machine ", currencyToString(credit))
+							coinsLeft($cv) := $cl - 1
+							display := concat("Pick up your change! Credit on machine ", concat(toString(credit/100), " €"))
 						endseq
 					endif
 				endlet
@@ -245,38 +233,30 @@ definitions:
 				endseq
 		endif
 
-		// Set the number of coins in the machine
+	// Set the number of coins in the machine
 	// (after insertion/removal by the maintenance man)
 	rule r_setCoins =
 		let($cv = insertedCoin, $n = numberOfCoins) in
-			let($cl = coinsLeft($cv), $mq = maxInseartableCoinsQuantity($cv)) in
-				if($n < $mq) then
-					seq
-						coinsLeft($cv) := $cl + $n
-						display := concat("Coins left ", toString(coinsLeft($cv)))
-					endseq
-				else
-					display := "MAX INSERTABLE COINS OVERBOUNDED"
-				endif
-
-				// Doesn't work on Natural minus
-				/*if($cl < $mq) then // Here $mq is the size of the pocket for each coin size
-					if($mq - $cl > $n) then
-						// If amount of insertedCoins is lower than space available, insert only money available
-						seq
-							coinsLeft($cv) := $cl + $n
-							display := concat("POCKET FULLFILLED WITH ", $n)
-						endseq
-					else
-						// Else refill completely pocket for coin of type $cv
-						seq
-							coinsLeft($cv) := $mq
-							display := "POCKET FULLFILLED"
-						endseq
-					endif
+			let($cl = coinsLeft($cv), $mq = maxSpaceAvailable($cv)) in
+				if($cl < $mq) then // Here $mq is the size of the pocket for each coin size
+					let($free_space = $mq - $cl) in
+						if($free_space > $n) then
+							// If amount of insertedCoins is lower than space available, insert only money available
+							par
+								coinsLeft($cv) := $cl + $n
+								display := "POCKET NOT COMPLETELY FULLFILLED"
+							endpar
+						else
+							// Else refill completely pocket for coin of type $cv
+							par
+								coinsLeft($cv) := $mq
+								display := "POCKET FULLFILLED WITH EXCEED MONEY"
+							endpar
+						endif
+					endlet
 				else
 					display := "MONEY POCKET ALREADY FULL"
-				endif*/
+				endif
 			endlet
 		endlet
 
@@ -340,9 +320,9 @@ definitions:
 	rule r_maintainAction =
 		if(state = MAINTAINING) then
 			switch(maintainAction)
-				case FINISH:	r_finish[]
-				case FILL:	 	r_fill[]
-				case SET_COINS:	r_setCoins[]
+				case FINISH:		r_finish[]
+				case FILL:	 		r_fill[]
+				case FILL_COINS:	r_setCoins[]
 			endswitch
 		endif
 
@@ -366,27 +346,27 @@ default init initial_state:
 	function display = "Waiting..."
 	function coinsLeft ($cv in CoinValue) =
 		switch($cv)
-			case   5 :  5n
-			case  10 : 20n
-			case  20 : 20n
-			case  50 : 20n
-			case 100 : 10n
-			case 200 :  5n
+			case   5 :  5
+			case  10 : 10
+			case  20 : 10
+			case  50 : 10
+			case 100 : 10
+			case 200 :  2
 		endswitch
-	function maxInseartableCoinsQuantity ($cv in CoinValue) =
+	function maxSpaceAvailable ($cv in CoinValue) =
 		switch($cv)
-			case   5 : 20n
-			case  10 : 20n
-			case  20 : 20n
-			case  50 : 10n
-			case 100 : 10n
-			case 200 : 	5n
+			case   5 : 30
+			case  10 : 30
+			case  20 : 30
+			case  50 : 30
+			case 100 : 20
+			case 200 : 10
 		endswitch
 	function credit = 0
 	function quantityLeft($i in Ingredient) =
 		switch($i)
-			case PLASTIC_GLASS	: 1000
-			case WATER			:  	80
+			case PLASTIC_GLASS	: 	35
+			case WATER			:  	25
 			otherwise 			   	40
 		endswitch
 
